a381bbe0599869404db649d299fec68dce2ad2b5
george <george@georges-MacBook-Pro.local>
2022-02-23 00:45:00 +0200

    adding users list to home page login and avatar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import UsersResults from '../components/users/UsersResults'
+
 
 function Home() {
   return (
-      <div>
-    <h1 className="text-6xl">Welcome</h1>
-    {process.env.REACT_APP_GITHUB_TOKEN}
-    </div>
+    <>
+    
+    <UsersResults />
+    </>
   )
 }
 
