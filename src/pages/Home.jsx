486b487b6012bdc7e5539f79b2151dd0a8e09d50
george <george@georges-MacBook-Pro.local>
2022-02-23 18:48:21 +0200

    adding user search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import UsersResults from '../components/users/UsersResults'
+import UserSeach from '../components/users/UserSearch'
 
 
 function Home() {
   return (
     <>
-    
+    <UserSeach /> 
     <UsersResults />
     </>
   )
