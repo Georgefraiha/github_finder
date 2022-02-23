d23db19c996be0c28297be0621d674130e1f6457
george <george@georges-MacBook-Pro.local>
2022-02-23 17:29:30 +0200

    adding updates before implementing search

--- src/components/users/UsersResults.jsx
+++ src/components/users/UsersResults.jsx
@@ -1,34 +1,20 @@
 import React from 'react'
-import {useEffect, useState} from 'react'
+import {useContext} from 'react'
 import Spinner from '../layouts/Spinner'
 import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContext'
 
 function UsersResults() {
+const {users, loading } = useContext(GithubContext)
+  // useEffect(()=>{
+  //   fetchUsers()
+  // }, [])
 
-  const [users, setUsers]=useState([])
-  const [loading, setLoading]=useState(true)
-
-
-  useEffect(()=>{
-    fetchUsers()
-  }, [])
-
-  const fetchUsers = async () =>{
-    // const response= await  fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
-    //   headers: {
-    //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-    //   }
-    // })
-
-    const response= await  fetch(`${process.env.REACT_APP_GITHUB_URL}/users`)
-    const data= await response.json()
-    setUsers(data)
-    setLoading(false)
-  }
+  
   if (!loading){
   return (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>{users.map((user)=>(
-      <UserItem key={user.id} user={user}  />
+      <UserItem key= {user.id} user={user}  />
       ))}</div>
          )
     } else {
